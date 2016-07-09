0e2d54eefc361c84eca9f91e96c30645875506bd
LuisGonc <lcrgoncalves@gmail.com>
2016-07-09 20:31:23 +0100

    - Code is finished and working in vera plus.

--- gate_chime.lua
+++ gate_chime.lua
@@ -1,14 +1,26 @@
-local host, port = "127.0.0.1", 100
-local socket = require("socket")
-local tcp = assert(socket.tcp())
+--[[ function that sends the state of the gate throug a socket --]]
+function send_gate_status()
 
-tcp:connect(host, port);
---note the newline below
-tcp:send("hello world\n");
+    --[[ Configure the Gate Chime Ip Address and Port --]]
+    local gate_chime_host, gate_chime_port = "192.168.10.12", 8080
 
-while true do
-    local s, status, partial = tcp:receive()
-    print(s or partial)
-    if status == "closed" then break end
+    --[[ Create the socket for the connection --]]
+    local gate_chime_socket = require("socket")
+    local gate_chime_tcp = assert(gate_chime_socket.tcp())
+
+    gate_chime_tcp:connect(gate_chime_host, gate_chime_port);
+
+    --[[ Check the Gate status and send it to the Gate chime --]]
+    local gate_status = luup.variable_get("urn:micasaverde-com:serviceId:SecuritySensor1", "Tripped", 5)
+
+    if (gate_status == "0") then
+         gate_chime_tcp:send("the gate is closed");
+    else
+         gate_chime_tcp:send("the gate is open");
+    end
+
+    gate_chime_tcp:close()
+
 end
-tcp:close()
+
+send_gate_status()
